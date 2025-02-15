ea7c0002922e0ee534cf4336abe70c0d0ba2db4d
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-02-15 11:51:11 -0400

    Update iculess-qt6base.sh

--- iculess-qt6base.sh
+++ iculess-qt6base.sh
@@ -4,21 +4,6 @@ set -e
 
 ARCH="$(uname -m)"
 
-case "${ARCH}" in
-	"x86_64")
-		TARGETS_TO_BUILD="X86;AMDGPU"
-		EXT="zst"
-		;;
-	"aarch64")
-		TARGETS_TO_BUILD="AArch64;AMDGPU"
-		EXT="xz"
-		;;
-	*)
-		echo "Unsupported Arch: '${ARCH}'"
-		exit 1
-		;;
-esac
-
 git clone https://gitlab.archlinux.org/archlinux/packaging/packages/qt6-base qt6-base
 cd ./qt6-base
 
@@ -26,14 +11,26 @@ cd ./qt6-base
 sed -i -e "s/x86_64/${ARCH}/" \
 	-e 's/-DFEATURE_journald=ON/-DFEATURE_journald=OFF/' \
 	-e '/-DFEATURE_libproxy=ON \\/a\    -DFEATURE_icu=OFF \\' ./PKGBUILD
-if [ "${ARCH}" = "aarch64" ]; then
-	sed -i '/-DFEATURE_no_direct_extern_access=ON/d' ./PKGBUILD
-fi
+
+case "${ARCH}" in
+	"x86_64")
+		EXT="zst"
+		;;
+	"aarch64")
+		EXT="xz"
+		sed -i 's/-DFEATURE_no_direct_extern_access=ON/-DQT_FEATURE_sql_ibase=OFF/' ./PKGBUILD
+		;;
+	*)
+		echo "Unsupported Arch: '${ARCH}'"
+		exit 1
+		;;
+esac
+
 cat ./PKGBUILD
 
 makepkg -f --skippgpcheck
 ls -la
-rm -f qt6-base-docs-*.pkg.tar.*
+rm -f qt6-base-docs-*.pkg.tar.* qt6-base-debug-*.pkg.tar.*
 mv ./qt6-base-*.pkg.tar.${EXT} ../qt6-base-iculess-${ARCH}.pkg.tar.${EXT}
 cd ..
 rm -rf ./qt6-base
