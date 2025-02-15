a1c20061f71a6518cb5886e1b0654e5a691ad540
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-02-15 17:30:32 +0000

    Make qt6-base and libxml2 without ICU support (#3)
    This removes the 30 MiB libicudata lib dependency, which is rarely needed.

--- iculess-libxml2.sh
+++ iculess-libxml2.sh
@@ -1,34 +1,33 @@
 #!/bin/sh
 
-set -e
+set -ex
 
 ARCH="$(uname -m)"
-
-case "${ARCH}" in
-	"x86_64")
-		TARGETS_TO_BUILD="X86;AMDGPU"
-		EXT="zst"
-		;;
-	"aarch64")
-		TARGETS_TO_BUILD="AArch64;AMDGPU"
-		EXT="xz"
-		;;
-	*)
-		echo "Unsupported Arch: '${ARCH}'"
-		exit 1
-		;;
-esac
 
 git clone https://gitlab.archlinux.org/archlinux/packaging/packages/libxml2.git libxml2
 cd ./libxml2
 
 # remove the line that enables icu support
 sed -i '/--with-icu/d' ./PKGBUILD
+
+case "${ARCH}" in
+	"x86_64")
+		EXT="zst"
+		;;
+	"aarch64")
+		EXT="xz"
+		sed -i "s/x86_64/${ARCH}/" ./PKGBUILD
+		;;
+	*)
+		echo "Unsupported Arch: '${ARCH}'"
+		exit 1
+		;;
+esac
 cat ./PKGBUILD
 
 makepkg -f --skippgpcheck
 ls -la
-rm -f libxml2-docs-*.pkg.tar.*
+rm -f ./libxml2-docs-*.pkg.tar.* ./libxml2-debug-*-x86_64.pkg.tar.* 
 mv ./libxml2-*.pkg.tar.${EXT} ../libxml2-iculess-${ARCH}.pkg.tar.${EXT}
 cd ..
 rm -rf ./libxml2
--- iculess-qt6base.sh
+++ iculess-qt6base.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
-set -e
+set -ex
 
 ARCH="$(uname -m)"
 
+git clone https://gitlab.archlinux.org/archlinux/packaging/packages/qt6-base qt6-base
+cd ./qt6-base
+
+# remove the line that enables icu support
+sed -i -e "s/x86_64/${ARCH}/" \
+	-e 's/-DFEATURE_journald=ON/-DFEATURE_journald=OFF/' \
+	-e '/-DFEATURE_libproxy=ON \\/a\    -DFEATURE_icu=OFF \\' ./PKGBUILD
+
 case "${ARCH}" in
 	"x86_64")
-		TARGETS_TO_BUILD="X86;AMDGPU"
 		EXT="zst"
 		;;
 	"aarch64")
-		TARGETS_TO_BUILD="AArch64;AMDGPU"
 		EXT="xz"
+		sed -i 's/-DFEATURE_no_direct_extern_access=ON/-DQT_FEATURE_sql_ibase=OFF/' ./PKGBUILD
 		;;
 	*)
 		echo "Unsupported Arch: '${ARCH}'"
@@ -19,17 +26,11 @@ case "${ARCH}" in
 		;;
 esac
 
-git clone https://gitlab.archlinux.org/archlinux/packaging/packages/qt6-base qt6-base
-cd ./qt6-base
-
-# remove the line that enables icu support
-sed -i -e 's/-DFEATURE_journald=ON/-DFEATURE_journald=OFF/' \
-	-e '/-DFEATURE_libproxy=ON \\/a\    -DFEATURE_icu=OFF \\' ./PKGBUILD
 cat ./PKGBUILD
 
 makepkg -f --skippgpcheck
 ls -la
-rm -f qt6-base-docs-*.pkg.tar.*
+rm -fv qt6-base-docs-*.pkg.tar.* qt6-base-debug-*.pkg.tar.*
 mv ./qt6-base-*.pkg.tar.${EXT} ../qt6-base-iculess-${ARCH}.pkg.tar.${EXT}
 cd ..
 rm -rf ./qt6-base
