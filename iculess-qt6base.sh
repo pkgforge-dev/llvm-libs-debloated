f5c9c34bb2ea347b4ef95124e54f4d855ddee9a1
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-02-15 01:06:39 -0400

    opps2

--- iculess-qt6base.sh
+++ iculess-qt6base.sh
@@ -24,7 +24,7 @@ cd ./qt6-base
 
 # remove the line that enables icu support
 sed -i -e "s/x86_64/${ARCH}/" \
-	's/-DFEATURE_journald=ON/-DFEATURE_journald=OFF/' \
+	-e 's/-DFEATURE_journald=ON/-DFEATURE_journald=OFF/' \
 	-e '/-DFEATURE_libproxy=ON \\/a\    -DFEATURE_icu=OFF \\' ./PKGBUILD
 cat ./PKGBUILD
 
