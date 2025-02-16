b1d916c32e8699e87767108487c191d721b9b7b5
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-02-16 18:52:10 -0400

    skill issue

--- iculess-libxml2.sh
+++ iculess-libxml2.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-set -ex
+set -x
 
 ARCH="$(uname -m)"
 
 _build_libxml2() (
+	rm -rf ./libxml2 || true
 	git clone https://gitlab.archlinux.org/archlinux/packaging/packages/libxml2.git libxml2
 	cd ./libxml2
-	
+
 	# remove the line that enables icu support
 	sed -i '/--with-icu/d' ./PKGBUILD
-	
+
 	case "${ARCH}" in
 		"x86_64")
 			EXT="zst"
@@ -25,24 +26,23 @@ _build_libxml2() (
 			;;
 	esac
 	cat ./PKGBUILD
-	
-	makepkg -f --skippgpcheck
+
+	makepkg -f --skippgpcheck || return 1
 	ls -la
-	rm -f ./libxml2-docs-*.pkg.tar.* ./libxml2-debug-*-x86_64.pkg.tar.* 
-	mv ./libxml2-*.pkg.tar.${EXT} ../libxml2-iculess-${ARCH}.pkg.tar.${EXT}
+	rm -f ./libxml2-docs-*.pkg.tar.* ./libxml2-debug-*-x86_64.pkg.tar.*
+	mv ./libxml2-*.pkg.tar.${EXT} ../libxml2-iculess-${ARCH}.pkg.tar.${EXT} || return 1
 	cd ..
 	rm -rf ./libxml2
 )
 
 COUNT=0
 while [ "$COUNT" -le 5 ]; do
-    if ! _build_libxml2; then
-        rm -rf ./libxml2 || true
-        COUNT=$((COUNT + 1))
-	echo "Failed, trying $COUNT time"
-    else
-        break
-    fi
+	if ! _build_libxml2; then
+		COUNT=$((COUNT + 1))
+		echo "Failed, trying $COUNT time"
+	else
+		break
+	fi
 done
 
 if [ "$COUNT" -gt 5 ]; then
@@ -50,5 +50,5 @@ if [ "$COUNT" -gt 5 ]; then
 	exit 1
 fi
 
-	
+
 echo "All done!"
