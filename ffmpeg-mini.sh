981f7e8287bfe8902f1c0afeaeb32209733631fe
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-02-17 02:08:04 +0000

    add ffmpeg mini (#4)

---
+++ ffmpeg-mini.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+set -ex
+
+sudo pacman -S --noconfirm \
+  aom \
+  glslang \
+  gsm \
+  jack \
+  libass \
+  libavc1394 \
+  libbluray \
+  libbs2b \
+  libdvdnav \
+  libdvdread \
+  libiec61883 \
+  libjxl \
+  libmodplug \
+  libopenmpt \
+  libplacebo \
+  libraw1394 \
+  libsoxr \
+  libssh \
+  libtheora \
+  libva \
+  libvdpau \
+  libvpx \
+  libwebp \
+  ocl-icd \
+  onevpl \
+  opencore-amr \
+  rav1e \
+  rubberband \
+  sdl2 \
+  snappy \
+  speex \
+  srt \
+  v4l-utils \
+  vapoursynth \
+  vid.stab \
+  vulkan-icd-loader \
+  x264 \
+  xvidcore \
+  zeromq \
+  zimg
+
+ARCH="$(uname -m)"
+case "${ARCH}" in
+	"x86_64")
+		EXT="zst"
+		sudo pacman -S --noconfirm svt-av1 vmaf
+		git clone https://gitlab.archlinux.org/archlinux/packaging/packages/ffmpeg.git ffmpeg
+		cd ./ffmpeg
+		;;
+	"aarch64")
+		EXT="xz"
+		git clone --depth 1 https://github.com/archlinuxarm/PKGBUILDs.git PKGBUILDs
+		mv ./PKGBUILDs/extra/ffmpeg ./
+		cd ./ffmpeg
+		sed -i "s/x86_64/${ARCH}/" ./PKGBUILD
+		;;
+	*)
+		echo "Unsupported Arch: '${ARCH}'"
+		exit 1
+		;;
+esac
+
+# remove x265 support and AV1 encoding support
+sed -i -e '/x265/d' \
+	-e '/librav1e/d' \
+	-e '/--enable-libsvtav1/d' ./PKGBUILD
+
+cat ./PKGBUILD
+
+makepkg -f --skippgpcheck
+ls -la
+rm -f ./ffmpeg-docs-*.pkg.tar.* ./ffmpeg-debug-*.pkg.tar.*
+mv ./ffmpeg-*.pkg.tar.${EXT} ../ffmpeg-mini-${ARCH}.pkg.tar.${EXT}
+cd ..
+rm -rf ./ffmpeg
+echo "All done!"
--- iculess-libxml2.sh
+++ iculess-libxml2.sh
@@ -1,34 +1,54 @@
 #!/bin/sh
 
-set -ex
+set -x
 
 ARCH="$(uname -m)"
 
-git clone https://gitlab.archlinux.org/archlinux/packaging/packages/libxml2.git libxml2
-cd ./libxml2
+_build_libxml2() (
+	rm -rf ./libxml2 || true
+	git clone https://gitlab.archlinux.org/archlinux/packaging/packages/libxml2.git libxml2
+	cd ./libxml2
 
-# remove the line that enables icu support
-sed -i '/--with-icu/d' ./PKGBUILD
+	# remove the line that enables icu support
+	sed -i '/--with-icu/d' ./PKGBUILD
 
-case "${ARCH}" in
-	"x86_64")
-		EXT="zst"
-		;;
-	"aarch64")
-		EXT="xz"
-		sed -i "s/x86_64/${ARCH}/" ./PKGBUILD
-		;;
-	*)
-		echo "Unsupported Arch: '${ARCH}'"
-		exit 1
-		;;
-esac
-cat ./PKGBUILD
+	case "${ARCH}" in
+		"x86_64")
+			EXT="zst"
+			;;
+		"aarch64")
+			EXT="xz"
+			sed -i "s/x86_64/${ARCH}/" ./PKGBUILD
+			;;
+		*)
+			echo "Unsupported Arch: '${ARCH}'"
+			exit 1
+			;;
+	esac
+	cat ./PKGBUILD
 
-makepkg -f --skippgpcheck
-ls -la
-rm -f ./libxml2-docs-*.pkg.tar.* ./libxml2-debug-*-x86_64.pkg.tar.* 
-mv ./libxml2-*.pkg.tar.${EXT} ../libxml2-iculess-${ARCH}.pkg.tar.${EXT}
-cd ..
-rm -rf ./libxml2
+	makepkg -f --skippgpcheck || return 1
+	ls -la
+	rm -f ./libxml2-docs-*.pkg.tar.* ./libxml2-debug-*-x86_64.pkg.tar.*
+	mv ./libxml2-*.pkg.tar.${EXT} ../libxml2-iculess-${ARCH}.pkg.tar.${EXT} || return 1
+	cd ..
+	rm -rf ./libxml2
+)
+
+COUNT=0
+while [ "$COUNT" -le 5 ]; do
+	if ! _build_libxml2; then
+		COUNT=$((COUNT + 1))
+		echo "Failed, trying $COUNT time"
+	else
+		break
+	fi
+done
+
+if [ "$COUNT" -gt 5 ]; then
+	echo "Failed to build libxml2 5 times"
+	exit 1
+fi
+
+
 echo "All done!"
