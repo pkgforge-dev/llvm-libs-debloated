1d72f3fa33c773c50907da1f243d62ef04eeafde
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-02-21 19:47:46 -0400

    disable `vapoursynth` from ffmpeg

--- ffmpeg-mini.sh
+++ ffmpeg-mini.sh
@@ -68,7 +68,8 @@ esac
 # remove x265 support and AV1 encoding support
 sed -i -e '/x265/d' \
 	-e '/librav1e/d' \
-	-e '/--enable-libsvtav1/d' ./PKGBUILD
+	-e '/--enable-libsvtav1/d' \
+	-e '/--enable-vapoursynth/d' ./PKGBUILD
 
 cat ./PKGBUILD
 
