96d326e57c58973447760f22c684a70b728ce3e4
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-04-15 12:20:21 +0000

    add `mesa-mini`

---
+++ mesa-mini.sh
@@ -0,0 +1,98 @@
+#!/bin/sh
+
+set -ex
+
+ARCH="$(uname -m)"
+
+echo -S --noconfirm \
+	clang \
+	directx-headers \
+	expat \
+	gcc-libs \
+	glibc \
+	libdrm \
+	libelf \
+	libglvnd \
+	libpng \
+	libva \
+	libvdpau \
+	libx11 \
+	libxcb \
+	libxext \
+	libxml2 \
+	libxrandr \
+	libxshmfence \
+	libxxf86vm \
+	llvm \
+	llvm-libs \
+	lm_sensors \
+	rust \
+	spirv-llvm-translator \
+	spirv-tools \
+	systemd-libs \
+	vulkan-icd-loader \
+	wayland \
+	xcb-util-keysyms \
+	cbindgen \
+	clang \
+	cmake \
+	elfutils \
+	glslang \
+	libclc \
+	meson \
+	python-mako \
+	python-packaging \
+	python-ply \
+	python-yaml \
+	rust-bindgen \
+	wayland-protocols \
+	xorgproto \
+	valgrind \
+	python-sphinx \
+	python-sphinx-hawkmoth
+
+
+# debloat mesa
+case "${ARCH}" in
+	"x86_64")
+		EXT="zst"
+		git clone https://gitlab.archlinux.org/archlinux/packaging/packages/mesa.git ./mesa
+		cd ./mesa
+		;;
+	"aarch64")
+		EXT="xz"
+		git clone https://github.com/archlinuxarm/PKGBUILDs ./mesa
+		cd ./mesa
+		mv -v ./extra/mesa/* ./extra/mesa/.* ./
+		;;
+	*)
+		echo "Unsupported Arch: '${ARCH}'"
+		exit 1
+		;;
+esac
+
+sed -i -e 's/r300,//' \
+	-e 's/svga,//' \
+	-e 's/softpipe,//' \
+	-e 's/gallium-nine=true/gallium-nine=false/' \
+	-e 's/-g1/-g0 -Os/g' ./PKGBUILD
+
+cat ./PKGBUILD
+
+makepkg -f --skippgpcheck
+ls -la
+rm -fv *-docs-*.pkg.tar.* *-debug-*.pkg.tar.*
+mv -v ./mesa-*.pkg.tar.${EXT}           ../mesa-mini-${ARCH}.pkg.tar.${EXT}
+mv -v ./vulkan-radeon-*.pkg.tar.${EXT}  ../vulkan-radeon-mini-${ARCH}.pkg.tar.${EXT}
+mv -v ./vulkan-nouveau-*.pkg.tar.${EXT} ../vulkan-nouveau-mini-${ARCH}.pkg.tar.${EXT}
+
+if [ "$ARCH" = 'x86_64' ]; then
+	mv -v ./vulkan-intel-*.pkg.tar.${EXT} ../vulkan-intel-mini-${ARCH}.pkg.tar.${EXT}
+else
+	mv -v ./vulkan-broadcom  ../vulkan-broadcom-mini-${ARCH}.pkg.tar.${EXT}
+	mv -v ./vulkan-panfrost  ../vulkan-panfrost-mini-${ARCH}.pkg.tar.${EXT}
+	mv -v ./vulkan-freedreno ../vulkan-freedreno-mini-${ARCH}.pkg.tar.${EXT}
+fi
+cd ..
+rm -rf ./mesa
+echo "All done!"
