e6f1f57d821fa1f6ffe2777395865f2be933613d
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-04-15 05:23:46 -0400

    build `qt6base` with MinSizeRel

--- iculess-qt6base.sh
+++ iculess-qt6base.sh
@@ -9,6 +9,7 @@ cd ./qt6-base
 
 # remove the line that enables icu support
 sed -i -e "s/x86_64/${ARCH}/" \
+	-e 's/-DCMAKE_BUILD_TYPE=RelWithDebInfo/-DCMAKE_BUILD_TYPE=MinSizeRel/' \
 	-e 's/-DFEATURE_journald=ON/-DFEATURE_journald=OFF/' \
 	-e '/-DFEATURE_libproxy=ON \\/a\    -DFEATURE_icu=OFF \\' ./PKGBUILD
 
