cfe7682d7e02f2eabec314ec703d55eb1ca496de
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-05-24 07:55:18 +0000

    upstream fixed it (#13)

--- ffmpeg-mini.sh
+++ ffmpeg-mini.sh
@@ -67,7 +67,6 @@ esac
 
 # remove x265 support and AV1 encoding support
 sed -i -e '/x265/d' \
-	-e '/libv4l2/d' \
 	-e '/librav1e/d' \
 	-e 's/--enable-libsvtav1/--enable-small/' \
 	-e '/--enable-vapoursynth/d' ./PKGBUILD
