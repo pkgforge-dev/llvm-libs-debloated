7b8b34947d4bf64d9e5a7f1e4f6644777304428d
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-07-03 22:01:59 +0000

    add much smaller version of `intel-media-driver` (#20)

---
+++ intel-media-mini.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -ex
+
+ARCH="$(uname -m)"
+
+sudo pacman -S --noconfirm intel-gmmlib libva
+
+git clone https://gitlab.archlinux.org/archlinux/packaging/packages/intel-media-driver.git ./intel-media-driver
+cd ./intel-media-driver
+
+# Build with MinSizeRel for smaller lib
+sed -i -e 's|-DINSTALL_DRIVER_SYSCONF=OFF|-DINSTALL_DRIVER_SYSCONF=OFF -DBUILD_TYPE=MinSizeRel -DENABLE_NONFREE_KERNELS=OFF|' ./PKGBUILD
+
+case "${ARCH}" in
+	"x86_64")
+		EXT="zst"
+		;;
+	*)
+		echo "Unsupported Arch: '${ARCH}'"
+		exit 1
+		;;
+esac
+
+cat ./PKGBUILD
+
+makepkg -f --skippgpcheck
+ls -la
+rm -fv *-docs-*.pkg.tar.* *-debug-*.pkg.tar.*
+mv ./intel-*.pkg.tar.${EXT} ../intel-media-mini-${ARCH}.pkg.tar.${EXT}
+cd ..
+rm -rf ./intel-media-driver
+echo "All done!"
