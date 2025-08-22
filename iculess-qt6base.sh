d5a67ec98f7b54f8037787f624a3d7dcb29167cf
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-22 05:16:33 -0400

    refactor

--- iculess-qt6base.sh
+++ iculess-qt6base.sh
@@ -9,32 +9,34 @@ sed -i -e 's|-O2|-Os|' /etc/makepkg.conf
 git clone https://gitlab.archlinux.org/archlinux/packaging/packages/qt6-base qt6-base
 cd ./qt6-base
 
-# remove the line that enables icu support
-sed -i -e "s/x86_64/${ARCH}/" \
-	-e 's/-DCMAKE_BUILD_TYPE=RelWithDebInfo/-DCMAKE_BUILD_TYPE=MinSizeRel/' \
-	-e 's/-DFEATURE_journald=ON/-DFEATURE_journald=OFF/' \
-	-e '/-DFEATURE_libproxy=ON \\/a\    -DFEATURE_icu=OFF \\' ./PKGBUILD
-
-case "${ARCH}" in
-	"x86_64")
-		EXT="zst"
+case "$ARCH" in
+	'x86_64')
+		EXT='zst'
 		;;
-	"aarch64")
-		EXT="xz"
-		sed -i 's/-DFEATURE_no_direct_extern_access=ON/-DQT_FEATURE_sql_ibase=OFF/' ./PKGBUILD
+	'aarch64')
+		EXT='xz'
+		sed -i -e 's/-DFEATURE_no_direct_extern_access=ON/-DQT_FEATURE_sql_ibase=OFF/' ./PKGBUILD
 		;;
 	*)
-		echo "Unsupported Arch: '${ARCH}'"
+		echo "Unsupported Arch: '$ARCH'"
 		exit 1
 		;;
 esac
+sed -i -e "s/x86_64/$ARCH/" ./PKGBUILD
 
+# remove the line that enables icu support
+sed -i \
+	-e 's/-DCMAKE_BUILD_TYPE=RelWithDebInfo/-DCMAKE_BUILD_TYPE=MinSizeRel/' \
+	-e 's/-DFEATURE_journald=ON/-DFEATURE_journald=OFF/' \
+	-e '/-DFEATURE_libproxy=ON \\/a\    -DFEATURE_icu=OFF \\' ./PKGBUILD
 cat ./PKGBUILD
 
 makepkg -fs --noconfirm --skippgpcheck
 ls -la
-rm -fv qt6-base-docs-*.pkg.tar.* qt6-base-debug-*.pkg.tar.*
-mv ./qt6-base-*.pkg.tar.${EXT} ../qt6-base-iculess-${ARCH}.pkg.tar.${EXT}
+rm -fv ./*-docs-*.pkg.tar.* ./*-debug-*.pkg.tar.*
+mv -v ./qt6-base-*.pkg.tar."$EXT" ../qt6-base-iculess-"$ARCH".pkg.tar."$EXT"
 cd ..
 rm -rf ./qt6-base
+# add make package with new name while keeping older name to not break existing CIs
+cp -v ./qt6-base-iculess-"$ARCH".pkg.tar."$EXT" ./qt6-base-mini-"$ARCH".pkg.tar."$EXT"
 echo "All done!"
