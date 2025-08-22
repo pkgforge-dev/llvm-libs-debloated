7fcf5bef4c3dec01b19263c11ce99fe725c8d080
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-22 05:08:22 -0400

    refactor

--- mangohud-mini.sh
+++ mangohud-mini.sh
@@ -12,32 +12,29 @@ rm -rf ./mangohud-temp
 
 cd ./mangohud
 
-sed -i -e "s|x86_64|$(uname -m)|" ./PKGBUILD
-
-case "${ARCH}" in
-	"x86_64")
-		EXT="zst"
+case "$ARCH" in
+	'x86_64')
+		EXT='zst'
 		;;
-	"aarch64")
-		EXT="xz"
-
+	'aarch64')
+		EXT='xz'
 		# remove libxnvctrl since it is not possible in aarch64
-		sed -i -e "s|x86_64|$(uname -m)|" \
+		sed -i \
 			-e 's|-Dmangohudctl=true|-Dmangohudctl=true -Dwith_xnvctrl=disabled|' \
 			-e '/libxnvctrl/d' ./PKGBUILD
 		;;
 	*)
-		echo "Unsupported Arch: '${ARCH}'"
+		echo "Unsupported Arch: '$ARCH'"
 		exit 1
 		;;
 esac
-
+sed -i -e "s|x86_64|$(uname -m)|" ./PKGBUILD
 cat ./PKGBUILD
 
 makepkg -fs --noconfirm --skippgpcheck
 ls -la
-rm -fv mangohud-docs-*.pkg.tar.* mangohud-debug-*.pkg.tar.*
-mv ./mangohud-*.pkg.tar.${EXT} ../mangohud-pythonless-${ARCH}.pkg.tar.${EXT}
+rm -fv ./*-docs-*.pkg.tar.* ./*-debug-*.pkg.tar.*
+mv -v ./mangohud-*.pkg.tar."$EXT" ../mangohud-pythonless-"$ARCH".pkg.tar."$EXT"
 cd ..
 rm -rf ./mangohud
 echo "All done!"
